57a5ea6e104499e7c36317e3b0c89fb754616091
Roland Vink <roland.vink@bridgeinternationalacademies.com>
2019-01-17 10:49:51 +0000

    Add and use Monitor component

---
+++ src/components/Monitor.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="monitor">
+        <p>
+            This is the monitor.
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Monitor extends Vue {
+
+}
+
+</script>
+
+<style scoped lang="scss">
+.monitor {
+    background-color: rgb(0, 0, 0);
+    width: 100%;
+    height: 1080px;
+}
+</style>
