9958838fee3ab27a0176b8bcf3a91b3968c60818
rcvink <rrvink@gmail.com>
2019-02-16 17:05:15 +0000

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueResource from 'vue-resource';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
-new Vue({
+const vue = new Vue({
   router,
   render: (h) => h(App),
-}).$mount('#app');
+});
+
+Vue.use(VueResource);
+vue.$mount('#app');
