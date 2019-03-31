61345788feb00bf2b4152c0c5139d782e35522f9
rcvink <rrvink@gmail.com>
2019-03-31 17:54:10 +0100

    Remove shadowed var

--- src/models/State.ts
+++ src/models/State.ts
@@ -1,6 +1,5 @@
 import IScene from '@/models/IScene';
 import IState from '@/models/IState';
-import scenes from '@/static/scenes.json';
 
 export default class State implements IState {
     public scene: IScene;
@@ -8,7 +7,7 @@ export default class State implements IState {
     public statements: string[];
     public printChain: Promise<{}>;
 
-    constructor(story: IScene[]) {
+    constructor(scenes: IScene[]) {
         this.scenes = scenes;
         this.scene = this.scenes[0];
         this.statements = [];
