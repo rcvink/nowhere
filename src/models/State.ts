c7ef00cb2b0688fd87095cfab5df349e32693232
rcvink <rrvink@gmail.com>
2020-03-02 14:13:51 +0000

    don't instantiate State with scenes json

--- src/models/State.ts
+++ src/models/State.ts
@@ -2,15 +2,8 @@ import IScene from '@/models/IScene';
 import IState from '@/models/IState';
 
 export default class State implements IState {
-    public scene: IScene;
-    public scenes: IScene[];
-    public statements: string[];
-    public printChain: Promise<{}>;
-
-    constructor(scenes: IScene[]) {
-        this.scenes = scenes;
-        this.scene = this.scenes[0];
-        this.statements = [];
-        this.printChain = Promise.resolve({});
-    }
+    public scenes: IScene[] = [];
+    public scene!: IScene;
+    public statements: string[] = [];
+    public printChain: Promise<{}> = Promise.resolve({});
 }
